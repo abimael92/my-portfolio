16de985632440951eddf3748700ce8ae23750c03
Abimael Garcia <abimael1992@gmail.com>
2021-08-14 01:36:12 -0500

    update html styles

--- js/script.js
+++ js/script.js
@@ -1,47 +1,19 @@
 
 // Preloader
-
-window.addEventListener('load', function () {
-    document.querySelector('.preloader').classList.add('opacity-0');
-    setTimeout(function () {
-        document.querySelector('.preloader').style.display = 'none';
-    }, 1000);
-});
+if ((document.querySelector('.preloader'))) {
+    window.addEventListener('load', function () {
+        document.querySelector('.preloader').classList.add('opacity-0');
+        setTimeout(function () {
+            document.querySelector('.preloader').style.display = 'none';
+        }, 1000);
+    });
+}
 
 // iTyped 
-
-window.ityped.init(document.querySelector('.iTyped'), {
-    strings: ["I'm a Web Developer", 'I Love Html5', 'I Love CSS3', 'I Love Javascript'],
-    loop: true
-});
-
-// Portfolio Item Filter
-
-const filterContainer = document.querySelector('.portfolio-filter'),
-    filterBtns = filterContainer.children,
-    totalFilterBtn = filterBtns.length,
-    portfolioItems = document.querySelectorAll('.portfolio-item'),
-    totalPortfolioItem = portfolioItems.length;
-
-for (let i = 0; i < totalFilterBtn; i++) {
-    filterBtns[i].addEventListener("click", function () {
-        filterContainer.querySelector('.active').classList.remove('active');
-        this.classList.add("active");
-
-        const filterValue = this.getAttribute('data-filter');
-        for (let k = 0; k < totalPortfolioItem; k++) {
-            if (filterValue === portfolioItems[k].getAttribute('data-category')) {
-                portfolioItems[k].classList.remove('hide');
-                portfolioItems[k].classList.add('show');
-            } else {
-                portfolioItems[k].classList.remove('show');
-                portfolioItems[k].classList.add('hide');
-            }
-            if (filterValue === 'all') {
-                portfolioItems[k].classList.remove('hide');
-                portfolioItems[k].classList.add('show');
-            }
-        }
+if ((document.querySelector('.iTyped'))) {
+    window.ityped.init(document.querySelector('.iTyped'), {
+        strings: ["I'm a Web Developer", 'I Love Html5', 'I Love CSS3', 'I Love Javascript'],
+        loop: true
     });
 }
 
@@ -54,14 +26,6 @@ const lightbox = document.querySelector('.lightbox'),
     lightboxCounter = lightbox.querySelector('.caption-counter');
 
 let itemIndex = 0;
-
-for (let i = 0; i < totalPortfolioItem; i++) {
-    portfolioItems[i].addEventListener('click', function () {
-        itemIndex = i;
-        changeItem();
-        toggleLightbox();
-    });
-}
 
 function toggleLightbox() {
     lightbox.classList.toggle('open');
@@ -153,13 +117,15 @@ function updateNav(element) {
     }
 }
 
-document.querySelector('.hire-me').addEventListener('click', function () {
-    const sectionIndex = this.getAttribute('data-section-index');
-    addBackSectionClass(sectionIndex);
-    showSection(this);
-    updateNav(this);
-    removeBackSectionClass();
-});
+if (document.querySelector('.hire-me')) {
+    document.querySelector('.hire-me').addEventListener('click', function () {
+        const sectionIndex = this.getAttribute('data-section-index');
+        addBackSectionClass(sectionIndex);
+        showSection(this);
+        updateNav(this);
+        removeBackSectionClass();
+    });
+}
 
 function showSection(element) {
     for (let i = 0; i < totalSection; i++) {
--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -12,6 +12,17 @@ function setActiveStyle(color) {
     }
 }
 
+function setSkin() {
+
+    if (document.getElementById("dark").checked == true) {
+        document.getElementById("dark").checked = true;
+        document.getElementById("light").checked = false;
+    } else {
+        document.getElementById("light").checked = true;
+        document.getElementById("dark").checked = false;
+    }
+}
+
 // body skin
 
 const bodySkin = document.querySelectorAll('.body-skin'),
@@ -19,18 +30,14 @@ const bodySkin = document.querySelectorAll('.body-skin'),
 
 for (let i = 0; i < totalBodySkin; i++) {
     bodySkin[i].addEventListener('change', function () {
+        console.log(this);
         if (this.value !== 'dark') {
             document.body.className = '';
-
         } else {
             document.body.className = 'dark';
         }
     });
 }
-
-window.onload = function () {
-    document.body.className = 'dark';
-};
 
 document.querySelector('.toggle-style-switcher').addEventListener('click', () => {
     document.querySelector('.style-switcher').classList.toggle('open');
